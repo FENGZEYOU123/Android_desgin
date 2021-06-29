55b18132ae9387205c05ef09e7b545468ae021a4
FENGZEYOU123 <35758413+FENGZEYOU123@users.noreply.github.com>
2021-06-29 18:02:50 +0800

    debug

--- project/app/src/main/java/com/yfz/main/behaviorPattern/strategy/StrategyContext.java
+++ project/app/src/main/java/com/yfz/main/behaviorPattern/strategy/StrategyContext.java
@@ -1,7 +1,7 @@
 package com.yfz.main.behaviorPattern.strategy;
 
 /**
- * 创建Context类
+ * 创建Context类-理解为使用策略的客户端
  */
 public class StrategyContext {
     public StrategyInterface mStrategyInterface;
